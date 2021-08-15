f76628ef658ce157e2b5304bf8f6f7801d0ac063
Leonardo Santos <leonardojdossantos@gmail.com>
2021-08-15 14:13:17 -0300

    feat: add geolocation testing function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords);
+    });
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
