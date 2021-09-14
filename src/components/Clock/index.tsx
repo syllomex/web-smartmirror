a9252692a7848f59d048b4d753504ceb73c9150b
Leonardo Santos <leonardojdossantos@gmail.com>
2021-09-14 19:51:45 -0300

    feat: add clock component

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-import styles from "@/styles/clock.module.css";
-import { capitalize } from "@/utils";
+import styles from '@/styles/clock.module.css';
+import { capitalize } from '@/utils';
 
 type Props = {
   temperature?: number;
-  direction?: "row" | "column";
+  direction?: 'row' | 'column';
 };
 
 const options: { weekStartsOn: 0 | 1 | 2 | 3 | 4 | 5 | 6; locale: Locale } = {
@@ -19,9 +19,9 @@ const options: { weekStartsOn: 0 | 1 | 2 | 3 | 4 | 5 | 6; locale: Locale } = {
 function getDateTime() {
   const now = new Date();
 
-  const time = format(now, "p", options);
+  const time = format(now, 'p', options);
 
-  const date = capitalize(format(now, "EEEE - P", options));
+  const date = capitalize(format(now, 'EEEE - P', options));
 
   return {
     time,
@@ -29,7 +29,7 @@ function getDateTime() {
   };
 }
 
-export default function Clock({ temperature, direction = "column" }: Props) {
+export default function Clock({ temperature, direction = 'column' }: Props) {
   const [dateTime, setDateTime] = useState<{ time: string; date: string }>(
     getDateTime()
   );
@@ -44,10 +44,16 @@ export default function Clock({ temperature, direction = "column" }: Props) {
     <div className={styles.container}>
       <div
         className={
-          direction === "column" ? styles.contentContainer : styles.rowContainer
+          direction === 'column' ? styles.contentContainer : styles.rowContainer
         }
       >
-        <div className={direction === "column" ? styles.dateTimeContainer : styles.dateTimeContainerRow}>
+        <div
+          className={
+            direction === 'column'
+              ? styles.dateTimeContainer
+              : styles.dateTimeContainerRow
+          }
+        >
           <span className={styles.time}>{dateTime.time}</span>
           <span className={styles.date}>{dateTime.date}</span>
         </div>
